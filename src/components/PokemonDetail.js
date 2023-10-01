92022b4eeb4dbc412b943dec6b6fac3321cf20f3
Ronito Asinjo Jr <ronitoa@corebridge.net>
2023-10-01 18:07:12 +0800

    Added assets and pokedex.json, Added routes in App components, initial creation of PokemonList and PokemonDetail components

---
+++ src/components/PokemonDetail.js
@@ -0,0 +1,9 @@
+import '../styles/PokemonDetail.css';
+
+const PokemonDetail = props => {
+  return (
+      <div className="pokemon-detail"></div>
+  );
+}
+
+export default PokemonDetail;
---
+++ src/components/PokemonList.js
@@ -0,0 +1,9 @@
+import '../styles/PokemonList.css';
+
+const PokemonList = props => {
+    return (
+      <div className="pokedex-grid"></div>
+    );
+};
+
+export default PokemonList;
